bf5ba795cadc9bb9f370f3359e0dd6a569123673
daridev <hernandezdarifrancisco@gmail.com>
2021-12-21 19:27:51 -0600

    Responsive about us images

--- code/index.js
+++ code/index.js
@@ -2,6 +2,7 @@ var menu_button = document.querySelector (".icons > .burger.icon")
 var menu = document.querySelector ("header > nav")
 var active_menu = false
 var hero_image = document.querySelector(".hero > figure.main-image > img")
+var gallery_images_elems = document.querySelectorAll(".gallery > figure > img")
 
 function toggle_menu () {
     menu.classList.toggle("active")
@@ -27,19 +28,21 @@ const circleType = new CircleType(document.querySelector (".cta > p.rounded"));
 circleType.dir(-1)
 circleType.radius(80);
 
-// Set hero image
+
+// Hero images list
 const hero_images = {
     "large": "./imgs/hero-large.png",
     "mediaum": "./imgs/hero-medium.png",
     "small": "./imgs/hero-small.png",
     "mobile": "./imgs/hero-mobile.png"
 }
-var current_image = ""
+var current_hero_image = ""
 
 function set_hero_image () {
     
-    var macth_image = ""
-    if (window.matchMedia("(max-width: 660px)").matches) {
+    // correct image
+    let macth_image = ""
+    if (window.matchMedia("(max-width: 680px)").matches) {
         macth_image = hero_images["mobile"]
     } else if (window.matchMedia("(max-width: 770px)").matches) {
         macth_image = hero_images["small"]
@@ -49,15 +52,66 @@ function set_hero_image () {
         macth_image = hero_images["large"]
     }
 
-    if (current_image != macth_image) {
-        current_image = macth_image
-        hero_image.setAttribute ("src", current_image)
+    // Apply image
+    if (current_hero_image != macth_image) {
+        current_hero_image = macth_image
+        hero_image.setAttribute ("src", current_hero_image)
     }
+}
 
+
+// Gallery images list
+const gallery_images = {
+    "desktop": [
+        "./imgs/gallery-1-desktop.png",
+        "./imgs/gallery-2-desktop.png",
+        "./imgs/gallery-3-desktop.png",
+        "./imgs/gallery-4-desktop.png"
+
+    ], 
+    "mobile_large": [
+        "./imgs/gallery-1-mobile-large.png",
+        "./imgs/gallery-2-mobile-large.png",
+        "./imgs/gallery-3-mobile-large.png",
+        "./imgs/gallery-4-mobile-large.png"
+    ],
+    "mobile_small": [
+        "./imgs/gallery-1-mobile-small.png",
+        "./imgs/gallery-2-mobile-small.png",
+        "./imgs/gallery-3-mobile-small.png",
+        "./imgs/gallery-4-mobile-small.png"
+    ]
+}
+var current_gallery_images = []
+
+function set_gallery_images () {
+
+    // Get images
+    let macth_images = []
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        macth_images = gallery_images["mobile_small"]
+    } else if (window.matchMedia("(max-width: 1200px)").matches) {
+        macth_images = gallery_images["mobile_large"]
+    } else {
+        macth_images = gallery_images["desktop"]
+    }
+    
+    // Apply images
+    if (current_gallery_images != macth_images) {
+        current_gallery_images = macth_images
+        for (index in Array.from (gallery_images_elems)) {
+            macth_image = macth_images[index]
+            gallery_images_elems [index].setAttribute ("src", macth_image)
+        }
+    }
 }
 
 // Set image when page load
 set_hero_image ()
+set_gallery_images ()
 
 // Update hero image
-window.addEventListener('resize', set_hero_image);
+window.addEventListener('resize', function (e) {
+    set_hero_image ()
+    set_gallery_images ()
+});
